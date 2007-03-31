e438d363a811d848fe255419646eb976e15ac56f
yjchen <yjchen@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2007-03-31 18:37:54 +0000

    Merge from trunk and include AZDock and AZBackground

---
+++ Etoile/Services/Private/AZDock/AZXWindowApp.h
@@ -0,0 +1,30 @@
+#import "AZDockApp.h"
+#import <X11/Xlib.h>
+
+/* Dock app can be a x window application, a gnustep application 
+ * or a file on harddisk */
+@interface AZXWindowApp: AZDockApp
+{
+  NSString *wm_class;
+  NSString *wm_instance;
+}
+
+- (id) initWithCommand: (NSString *) cmd
+       instance: (NSString *) instance class: (NSString *) class;
+- (id) initWithXWindow: (Window) win;
+
+/* Return NO is the win does not belong to this view */
+- (BOOL) acceptXWindow: (Window) win;
+
+/* Return YES if it has win and remove it successfully.
+ * If win is the group window, all x windows in this application 
+ * will be remove */
+- (BOOL) removeXWindow: (Window) win;
+
+/* Return number of XWindows */
+- (unsigned int) numberOfXWindows;
+
+- (NSString *) wmClass;
+- (NSString *) wmInstance;
+
+@end
