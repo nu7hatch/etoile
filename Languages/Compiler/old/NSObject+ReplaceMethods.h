a2791448e7848691ccfaeabd3469d44bf48a44da
theraven <theraven@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2008-10-24 18:07:30 +0000

    Removed stuff that shouldn't have gone in to stable.
    Merged changes from trunk, reviewed by qmathe:
    
    http://review.etoileos.com/r/32/

--- Languages/Compiler/old/NSObject+ReplaceMethods.h
+++
@@ -1,5 +0,0 @@
-#include <EtoileFoundation/EtoileFoundation.h>
-@interface NSObject (RuntimeHackery)
-+ (int) replaceMethodForSelector:(SEL)aSelector with:(IMP)aMethod;
-+ (int) addMethod:(IMP)aMethod forSelectorNamed:(char*)aSelector;
-@end
