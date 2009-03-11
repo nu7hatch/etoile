08c658cd7a6ceaf31ebaf3c48aca1746216b5e49
ericwa <ericwa@7b3f36aa-e6db-49c1-8b4c-66eac1997e64>
2009-03-11 05:37:30 +0000

    Merge Melodie from trunk

--- Services/User/Melodie/main.m
+++ Services/User/Melodie/main.m
@@ -1,21 +1,13 @@
 #import <EtoileFoundation/EtoileFoundation.h>
+#import <LanguageKit/LKCompiler.h>
 #import <AppKit/AppKit.h>
-#import <SmalltalkKit/SmalltalkKit.h>
-
-void load(NSString *file) {
-	NSLog(@"Loading %@", file);
-	[SmalltalkCompiler loadApplicationScriptNamed:file];
-}
 
 int main(int argc, char **argv)
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-	load(@"ETPlaylist");
-	load(@"MusicPlayerController");
-	load(@"MelodieController");
-	NSLog(@"Loaded smalltalk scripts.");
-	
+	[LKCompiler loadAllScriptsForApplication];
+
 	int ret = NSApplicationMain(argc, (const char **) argv);
 	[pool release];
 	return ret;
